6521de603b098239ab9f93ce7539b13da5890885
Artur Buja <artur.buja2@gmail.com>
2022-01-04 18:35:09 +0100

    adding game logic

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict';
 // selecting elements
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
@@ -11,14 +16,28 @@ const btnHold = document.querySelector('.btn--hold')
 score0El.textContent = '0';
 score1El.textContent = '0';
 diceEl.classList.add('hidden');
-
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', ()=>{
-// 1. Wygenerowanie randomowego 'dice'
+// 1. Wygenerowanie losowej wartości kostki
   const dice = Math.trunc(Math.random() * 6) + 1;
 //  2. Pokazanie kostki
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`
-//  3. jeśli wyszło 1, przejdź do kolejnego gracza
+//  3. Dodaj wartość kostki do aktualnych punktów
+  if(dice !== 1){
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore
+
+     } else{
+    //  4. jeśli wyszło 1, przejdź do kolejnego gracza
+    document.getElementById(`current--${activePlayer}`).textContent = '0';
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 })
